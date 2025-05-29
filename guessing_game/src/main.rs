5b1958acb2046a47a4fc53ed24e38d8abfb39e1d
0xlitf <0xlitf@gmail.com>
2025-05-29 15:28:20 +0800

    `

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,11 +1,34 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("guess the number");
-    println!("input the number:");
-    let mut guess = String::new();
+
+    let secret_number = rand::rng().random_range(1..=100);
+    println!("the secret number is: {secret_number}");
+
+    loop {
+        println!("input the number:");
+        let mut guess = String::new(); // 关联函数(associated function)，关联函数是类型本身所实现的一种函数
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
     
-    io::stdin().read_line(&mut guess).expect("failed to read line");
-    
-    println!("you guessed: {guess}");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("you guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
 }
