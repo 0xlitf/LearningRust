86398237be5a5cf7d71c048c27e7225a1899c6a2
0xlitf <0xlitf@gmail.com>
2025-06-03 17:25:01 +0800

    `

--- 9.panic/src/main.rs
+++ 9.panic/src/main.rs
@@ -1,3 +1,43 @@
+#![allow(unused)]
+use std::io::ErrorKind;
+
 fn main() {
-    println!("Hello, world!");
+    let v = vec![1, 2, 3];
+
+    // v[99];
+
+    use std::fs::File;
+    let f = File::open("hello.txt");
+    println!("{:?}", f);
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => panic!("Problem opening the file: {:?}", other_error),
+        },
+    };
+
+    let f = File::open("hello.txt");
+    let f = f.unwrap_or_else(|error| match error.kind() {
+        ErrorKind::NotFound => match File::create("hello.txt") {
+            Ok(fc) => fc,
+            Err(e) => panic!("Problem creating the file: {:?}", e),
+        },
+        other_error => panic!("Problem opening the file: {:?}", other_error),
+    });
+
+    let f = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+
 }
