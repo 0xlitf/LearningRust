1f760b57355cceabfe0264f713bea32a7343931f
0xlitf <0xlitf@gmail.com>
2025-06-04 13:58:16 +0800

    `

--- 10.generics/src/main.rs
+++ 10.generics/src/main.rs
@@ -1,3 +1,35 @@
+
+#![allow(unused)]
 fn main() {
-    println!("Hello, world!");
+    pub trait Summary {
+        // trait 体中可以有多个方法：一行一个方法签名且都以分号结尾。
+        fn summarize(&self) -> String;
+        fn publish(&self) -> String;
+    }
+
+    pub struct NewsArticle {
+        pub headline: String,
+        pub location: String,
+        pub author: String,
+        pub content: String,
+    }
+
+    impl Summary for NewsArticle {
+        fn summarize(&self) -> String {
+            format!("{}, by {} ({})", self.headline, self.author, self.location)
+        }
+    }
+
+    pub struct Tweet {
+        pub username: String,
+        pub content: String,
+        pub reply: bool,
+        pub retweet: bool,
+    }
+
+    impl Summary for Tweet {
+        fn summarize(&self) -> String {
+            format!("{}: {}", self.username, self.content)
+        }
+    }
 }
